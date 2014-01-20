b50751ed27cf09601e7781757ae4e73ac7a2f097
Aleksandar Blagotić <aca.blagotic@gmail.com>
2014-01-20 20:54:29 +0100

    Added support for changing vortex shape for specific users

--- R/plot.R
+++ R/plot.R
@@ -7,9 +7,11 @@
 ##' @param group additional vector for group affiliation (defaults to \code{NULL})
 ##' @param group.colors character vector for group colors (defaults to \code{NULL})
 ##' @param arrow.colors character vector for arrow colors (defaults to \code{NULL})
+##' @param user.focus logical or numeric vector with indices for users whose vertex shape will be changed on the sociogram according to the value provided in \code{user.focus.shape} argument (defaults to \code{NULL})
+##' @param user.focus.shape see \code{user.focus} (defaults to \code{csquare})
 ##' @import igraph
 ##' @export
-sociogram <- function(m, group = NULL, group.colors = NULL, arrow.colors = NULL) {
+sociogram <- function(m, group = NULL, group.colors = NULL, arrow.colors = NULL, user.focus = NULL, user.focus.shape = 'csquare') {
     stopifnot(inherits(m, 'moreno'))
     ## prepare data matrix
     d0 <- as.data.frame(t(apply(m$data, 1, function(x){
@@ -40,6 +42,14 @@ sociogram <- function(m, group = NULL, group.colors = NULL, arrow.colors = NULL)
         ## color vertex based on group
         V(g)$group <- group
         V(g)$color <- as.character(factor(group, labels = group.colors))
+    }
+
+    ## TODO: nodesize by votes
+
+    ## change plot vertex for specific users
+    if (!is.null(user.focus)) {
+        V(g)$shape <- 'circle'
+        V(g)[user.focus]$shape <- user.focus.shape
     }
 
     ## avoid node overlap (fruchterman.reingold ?)
